dd5155df52f8bb6532f8acf3716b7d036dbb99d6
Jose Ortiz <jose.ortiz@live.com>
2020-02-25 10:57:22 -0700

    Added download implementation and menu icon.

--- app/src/main/java/edu/cnm/deepdive/nasaapod/viewmodel/MainViewModel.java
+++ app/src/main/java/edu/cnm/deepdive/nasaapod/viewmodel/MainViewModel.java
@@ -14,16 +14,20 @@ import edu.cnm.deepdive.nasaapod.model.repository.ApodRepository;
 import edu.cnm.deepdive.nasaapod.service.ApodService;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.functions.Action;
 import io.reactivex.functions.Consumer;
 import io.reactivex.schedulers.Schedulers;
 import java.text.ParseException;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class MainViewModel extends AndroidViewModel implements LifecycleObserver {
 
   private final MutableLiveData<Apod> apod;
   private final MutableLiveData<Throwable> throwable;
+  private final MutableLiveData<Set<String>> permissions;
   private final CompositeDisposable pending;
   private final ApodRepository repository;
 
@@ -33,6 +37,7 @@ public class MainViewModel extends AndroidViewModel implements LifecycleObserver
     apod = new MutableLiveData<>();
     throwable = new MutableLiveData<>();
     pending = new CompositeDisposable();
+    permissions = new MutableLiveData<>(new HashSet<>());
     Date today = new Date();
     String formattedDate = ApodService.DATE_FORMATTER.format(today);
     try {
@@ -55,6 +60,24 @@ public class MainViewModel extends AndroidViewModel implements LifecycleObserver
     return throwable;
   }
 
+  public LiveData<Set<String>> getPermissions() {
+    return permissions;
+  }
+
+  public void grantPermission(String permission) {
+    Set<String> permissions = this.permissions.getValue();
+    if (permissions.add(permission)) {
+      this.permissions.setValue(permissions);
+    }
+  }
+
+  public void revokePermission(String permission) {
+    Set<String> permissions = this.permissions.getValue();
+    if (permissions.remove(permission)) {
+      this.permissions.setValue(permissions);
+    }
+  }
+
   public void setApodDate(Date date) {
     throwable.setValue(null);
     pending.add(
@@ -73,6 +96,18 @@ public class MainViewModel extends AndroidViewModel implements LifecycleObserver
         .observeOn(AndroidSchedulers.mainThread())
         .subscribe(
             pathConsumer,
+            throwable::setValue
+        )
+    );
+  }
+
+  public void downloadImage(@NonNull Apod apod, Action onSuccess) {
+    throwable.setValue(null);
+    pending.add(
+        repository.downloadImage(apod)
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe(
+            onSuccess,
             throwable::setValue
         )
     );
